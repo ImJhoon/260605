9f2970cd28e5b8dea156a834209271c3762dd077
ImJhoon <jhjh9907@gmail.com>
2026-06-05 10:37:16 +0900

    chore: node_modules 캐시 삭제 및 gitignore 적용

--- 01_review.js
+++ 01_review.js
@@ -1,9 +1,10 @@
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = dotenv.PORT ?? 3000;
 
 app.listen(PORT, () => {
   console.log(`${PORT}에서 실행 중`);
